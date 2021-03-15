25cd0b1b78405df75b213477fc0e224f6d0ea6c8
deodeepkunj <77825051+deodeepkunj@users.noreply.github.com>
2021-03-15 23:09:14 +0530

    final

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,32 +12,32 @@ const config = {
   appId: "1:850995411664:web:7ddc01d597846f65",
 };
 
-export const createUserProfileDocument = async (userAuth, additonalData) => {
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`user/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-
     try {
       await userRef.set({
         displayName,
         email,
         createdAt,
-        ...additonalData,
+        ...additionalData,
       });
     } catch (error) {
-      console.log("error craeting user", error.message);
+      console.log("error creating user", error.message);
     }
   }
+
   return userRef;
 };
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
